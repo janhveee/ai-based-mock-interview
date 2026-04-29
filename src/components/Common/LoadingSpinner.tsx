2f185b1f1c372b313487572db0680e8746de659d
Janhvee Laad <111445776+janhveee@users.noreply.github.com>
2026-04-30 01:04:58 +0530

    Add LoadingSpinner component for loading indication

---
+++ src/components/Common/LoadingSpinner.tsx
@@ -0,0 +1,7 @@
+export const LoadingSpinner = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="animate-spin rounded-full h-16 w-16 border-b-2 border-primary"></div>
+    </div>
+  );
+};
